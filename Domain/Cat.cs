ca83820e57aedfcfac6382099f87c6c4921b5c32
User <User@WIN-ND4IIRPNMNP>
2017-05-13 18:17:46 +0700

    1

---
+++ Domain/Cat.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Cat
+    {
+        private string _name;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (_name == null)
+                    _name = value;
+            }
+        }
+
+        public DateTime Age { get; }
+
+        private int Health = 10;
+
+        public ConsoleColor Color
+        {
+            get
+            {
+                if (Health <= 10 && Health >= 7)
+                    return ConsoleColor.Green;
+                else
+                if (Health < 7 && Health >= 3)
+                    return ConsoleColor.Yellow;
+                else
+                if (Health < 3 && Health >= 1)
+                    return ConsoleColor.Red;
+                else
+                if (Health < 1 && Health >= 0)
+                    return ConsoleColor.DarkGray;
+                else
+                    return ConsoleColor.Magenta;
+            }
+        }
+
+        public void Feed()
+        {
+            if (Health <= 9)
+                Health += 1;
+            else
+                Health = 10;
+
+        }
+
+        public void Punish()
+        {
+            if (Health >= 1)
+                Health -= 1;
+            else
+                Health = 0;
+        }
+
+        public Cat()
+        {
+            Age = DateTime.Now.AddMonths(-2);
+        }
+    }
+    
+}
---
+++ View/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+
+namespace View
+{
+    class Program
+    {
+        static void PrintCatName(Cat cat)
+        {
+            Console.Write(cat.Name, Console.ForegroundColor = cat.Color);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        static void PrintCatMenu(Cat cat)
+        {
+            Console.Write("\nМеню для ");
+            PrintCatName(cat);
+            Console.WriteLine("\n1: вывести имя кота\n2: покормить кота \n3: наказать кота\n4: узнать дату рождения \n5,e,exit: выйти из программы");
+        }
+
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat();
+
+            Console.Write("Введите имя кота ");
+            cat.Name = Console.ReadLine();
+
+            Console.Write("Поздравляем вас с покупкой ");
+            PrintCatName(cat);
+
+            bool exit = true;
+            while (exit)
+            {
+                PrintCatMenu(cat);
+
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        Console.Clear();
+                        PrintCatName(cat);
+                        Console.WriteLine();
+                        break;
+
+                    case "2":
+                        Console.Clear();
+                        cat.Feed();
+                        PrintCatName(cat);
+                        Console.WriteLine(": mrrrr~r~r~");
+                        break;
+
+                    case "3":
+                        Console.Clear();
+                        cat.Punish();
+                        PrintCatName(cat);
+                        Console.WriteLine(": sh-h-h-h");
+                        break;
+                    case "4":
+                        Console.Clear();
+                        Console.WriteLine(cat.Age.ToShortDateString());
+                        break;
+
+                    case "5":
+                    case "exit":
+                    case "e":
+                        exit = false;
+                        break;
+
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Вы ввели значение, не соответствующее пунктам меню. Попробуйте снова.");
+                        break;
+                }
+            }
+        }
+    }
+}
